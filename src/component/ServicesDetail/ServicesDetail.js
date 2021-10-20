f6dd2b07d244c177c5d04a522cdcab042d276e64
MD YOUSUF SHEIKH <sheikhyousuf702@gmail.co>
2021-10-20 18:16:40 +0600

    3day

--- src/component/ServicesDetail/ServicesDetail.js
+++ src/component/ServicesDetail/ServicesDetail.js
@@ -5,16 +5,32 @@ const ServicesDetail = (props) => {
     const { serviceId } = useParams();
     const [service, setService] = useState()
     useEffect(() => {
-        const url = `./service.json/${serviceId}`;
+        const url = `/services/service-${serviceId}.json`;
         fetch(url)
             .then(res => res.json())
             .then(data => setService(data))
     }, [])
-    console.log("hello", service);
+    console.log(service);
     return (
-        <div>
-            <h1 className="text-3xl text-center">Hello{serviceId}</h1>
-        </div>
+        <>
+            <div className="py-3 pb-5 bg-green-50 px-4">
+                <h1 className="text-4xl md:text-5xl pb-5 text-blue-400 text-center font-medium">{service?.name} <i className={`text-blue-400 ${service?.icon}`}></i> </h1>
+
+                <div className="grid py-5  lg:grid-cols-2">
+                    <div className="px-3">
+                        <img className="mx-auto rounded shadow-lg hover:shadow " src={service?.img} alt="" />
+                    </div>
+                    <div className="px-3">
+                        <h3 className="text-2xl text-justify pb-2 text-blue-400">{service?.subTitle}</h3>
+                        <p className="text-justify pb-2">{service?.descripton}</p>
+                    </div>
+
+                </div>
+
+            </div>
+            <div className="p-4 bg-blue-200 shadow-lg ">
+            </div>
+        </>
     );
 };
 
